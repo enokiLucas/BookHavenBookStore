df0ead920dbd2a166816e3a58860490e74068d79
Lucas Enoki <enoki.lucas@gmail.com>
2026-01-10 14:45:42 -0300

    Add the scripts for local storage of contact information, according to the example

---
+++ scripts/contact.js
@@ -0,0 +1,13 @@
+const submitButton = document.querySelector("#submit-contact-us");
+const name = document.querySelector("#name").value;
+const email = document.querySelector("#email").value;
+const msg = document.querySelector("#msg").value;
+
+const customerInfo = {
+  name,
+  email,
+  msg,
+};
+
+const keyValue = name;
+localStorage.setItem(keyValue, JSON.stringify(customerInfo));
