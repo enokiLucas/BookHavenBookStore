e62bda7fca3f55bc218bd78785f291f7d4bd8fa2
Lucas Enoki <enoki.lucas@gmail.com>
2026-01-10 10:19:20 -0300

    Implement the view cart button following the example

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,6 +1,8 @@
 const cartItems = JSON.parse(sessionStorage.getItem("cartItems")) || [];
 
 const addToCartButtons = document.querySelectorAll(".add-to-cart");
+const viewCartButton = document.querySelector("#view-cart-button");
+const cartModal = document.querySelector(".cart-modal");
 
 addToCartButtons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -10,10 +12,32 @@ addToCartButtons.forEach((button) => {
   });
 });
 
+viewCartButton.addEventListener("click", () => {
+  console.log("view cart");
+  openCartModel();
+  displayCartItems();
+});
+
 function addToCart(itemName) {
   cartItems.push(itemName);
   sessionStorage.setItem("cartItems", JSON.stringify(cartItems));
   showMessage("Item added to the cart: " + itemName);
+}
+
+function openCartModel() {
+  cartModal.style.display = "block";
+}
+
+function displayCartItems() {
+  const cartList = document.getElementById("cart-items");
+  cartList.innerHTML = "";
+  if (cartItems && cartItems.length > 0) {
+    for (var i = 0; i < cartItems.length; i++) {
+      var listItem = document.createElement("li");
+      listItem.textContent = cartItems[i];
+      cartList.appendChild(listItem);
+    }
+  }
 }
 
 function showMessage(msg) {
