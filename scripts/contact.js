48fb6009137444d385bdac3b929ca236645c730e
Lucas Enoki <enoki.lucas@gmail.com>
2026-01-11 13:19:24 -0300

    Fix the contact script

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,17 +1,18 @@
 const submitButton = document.querySelector("#submit-contact-us");
-const name = document.querySelector("#name").value;
-const email = document.querySelector("#email").value;
-const msg = document.querySelector("#msg").value;
-
-const customerInfo = {
-  name,
-  email,
-  msg,
-};
-
-const keyValue = name;
-localStorage.setItem(keyValue, JSON.stringify(customerInfo));
 
 submitButton.addEventListener("click", () => {
+  const name = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const msg = document.querySelector("#msg").value;
+
+  const customerInfo = {
+    name,
+    email,
+    msg,
+  };
+
+  const keyValue = name;
+  localStorage.setItem(keyValue, JSON.stringify(customerInfo));
+
   alert("Thank you for your message");
 });
